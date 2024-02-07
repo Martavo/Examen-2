412dd86203aec6aee11a7cab68dd9fd663788ee8
Marta Villalba <marta.villalba.orta@gmail.com>
2024-02-07 10:52:22 +0100

    Kata sobre el catan con menú

--- index.php
+++ index.php
@@ -1,20 +1,54 @@
-<?php
-include_once 'jugador.php';
+<?php 
+include_once'jugador.php';
 
-$jugador = new jugador("Pedro", 4, 2, 3, 6, 5);
+$recursos = array(
+    'madera' => 4,
+    'roca' => 2,
+    'cereales' => 3,
+    'arcilla' => 6,
+    'lana' => 5
+);
 
-echo $jugador->comprarCamino() . "\n";
-echo $jugador->mostrarRecursos();
+$jugador = new Jugador("Azul", $recursos);
 
-echo $jugador->comprarPoblado() . "\n";
-echo $jugador->mostrarRecursos();
+do {
+    mostrarMenu();
+    $opcion = trim(fgets(STDIN));
 
-echo $jugador->comprarCiudad() . "\n";
-echo $jugador->mostrarRecursos();
+    switch($opcion){
+        case 1: 
+            echo $jugador->comprarCamino() . "\n";
+            echo $jugador->mostrarRecursos();
+            break;
+        case 2: 
+            echo $jugador->comprarPoblado() . "\n";
+            echo $jugador->mostrarRecursos();
+            break;
+        case 3: 
+            echo $jugador->comprarCiudad() . "\n";
+            echo $jugador->mostrarRecursos();
+            break;
+        case 4: 
+            echo $jugador->comprarCartaDesarrollo() . "\n";
+            echo $jugador->mostrarRecursos();
+            break;
+        case 5: 
+            echo $jugador->mostrarRecursos();
+            break;
+        case 0:
+            echo "Gracias por usar la aplicación";
+            break;
+        default:
+            echo "Opción inválida, por favor elige una opción correcta\n";
+    }
+} while($opcion != 0);
 
-echo $jugador->comprarCartaDesarrollo() . "\n";
-echo $jugador->mostrarRecursos();
-
-
-
-?>
+function mostrarMenu() {
+    echo "Lista de acciones:\n";
+    echo "1. Comprar camino\n";
+    echo "2. Comprar poblado\n";
+    echo "3. Comprar ciudad\n";
+    echo "4. Comprar carta de desarrollo\n";
+    echo "5. Ver recursos\n";
+    echo "0. Acabar el juego\n";
+}
--- jugador.php
+++ jugador.php
@@ -1,28 +1,18 @@
 <?php
 
 class Jugador {
-    private string $nombre;
-    private int $madera;
-    private int $roca;
-    private int $cereales;
-    private int $arcilla;
-    private int $lana;
+    private string $color;
+    private array $recursos;
 
-    public function __construct($nombre, $madera, $roca, $cereales, $arcilla, $lana) {
-        $this->nombre = $nombre;
-        $this->madera = $madera;
-        $this->roca = $roca;
-        $this->cereales = $cereales;
-        $this->arcilla = $arcilla;
-        $this->lana = $lana;
+    public function __construct($color, $recursos) {
+        $this->color = $color;
+        $this->recursos = $recursos;
     }
 
     public function comprarCamino() {
-        if ($this->madera >= 1 && $this->arcilla >= 1) {
-
-            $this->madera -= 1;
-            $this->arcilla -= 1;
-
+        if ($this->recursos['madera'] >= 1 && $this->recursos['arcilla'] >= 1) {
+            $this->recursos['madera'] -= 1;
+            $this->recursos['arcilla'] -= 1;
             return "Has comprado un camino.";
         } else {
             return "No tienes suficientes recursos para comprar un camino.";
@@ -30,50 +20,39 @@ class Jugador {
     }
 
     public function comprarPoblado() {
-        if ($this->madera >= 1 && $this->arcilla >= 1 && $this->cereales >= 1 && $this->lana >= 1) {
-            
-            $this->madera -= 1;
-            $this->cereales -= 1;
-            $this->arcilla -= 1;
-            $this->lana -= 1;
-            
+        if ($this->recursos['madera'] >= 1 && $this->recursos['arcilla'] >= 1 && $this->recursos['cereales'] >= 1 && $this->recursos['lana'] >= 1) {
+            $this->recursos['madera'] -= 1;
+            $this->recursos['cereales'] -= 1;
+            $this->recursos['arcilla'] -= 1;
+            $this->recursos['lana'] -= 1;
             return "Has comprado un poblado.";
         } else {
-            return "No tienes suficientes recursos para comprar un camino.";
+            return "No tienes suficientes recursos para comprar un poblado.";
         }
     }
 
     public function comprarCiudad() {
-        if ($this->roca >= 3 && $this->cereales >= 2 ) {
-            
-            $this->roca -= 3;
-            $this->cereales -= 2;
-
-            
+        if ($this->recursos['roca'] >= 3 && $this->recursos['cereales'] >= 2) {
+            $this->recursos['roca'] -= 3;
+            $this->recursos['cereales'] -= 2;
             return "Has comprado una ciudad.";
         } else {
-            return "No tienes suficientes recursos para comprar un camino.";
+            return "No tienes suficientes recursos para comprar una ciudad.";
         }
     }
 
     public function comprarCartaDesarrollo() {
-        if ($this->roca >= 1 && $this->cereales >= 1 && $this->lana >= 1) {
-            
-            $this->roca -= 1;
-            $this->cereales -= 1;
-            $this->lana -= 1;
-            
+        if ($this->recursos['roca'] >= 1 && $this->recursos['cereales'] >= 1 && $this->recursos['lana'] >= 1) {
+            $this->recursos['roca'] -= 1;
+            $this->recursos['cereales'] -= 1;
+            $this->recursos['lana'] -= 1;
             return "Has comprado una carta de desarrollo.";
         } else {
-            return "No tienes suficientes recursos para comprar un camino.";
+            return "No tienes suficientes recursos para comprar una carta de desarrollo.";
         }
     }
 
     public function mostrarRecursos(){
-        return "Los recursos disponibles son: madera {$this->madera}, roca {$this->roca}, arcilla {$this->arcilla}, cereales {$this->cereales}, lana {$this->lana}. \n ";
+        return "Los recursos disponibles son: madera {$this->recursos['madera']}, roca {$this->recursos['roca']}, arcilla {$this->recursos['arcilla']}, cereales {$this->recursos['cereales']}, lana {$this->recursos['lana']}.\n";
     }
-
-
 }
-
-?>
